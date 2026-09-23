18b4138cba1b96396fbb443e65df597d71d1f0ed
David Queipo <DAVIDQUEIPO67@GMAIL.COM>
2026-09-22 22:17:57 -0400

    Update y value in Vec3 struct initialization

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ struct Vec3 {
 int main(void) {
     struct Vec3 p;
     p.x = 0.0;
-    p.y = 0.0;
+    p.y = 90.0;
     p.z = 1000.0;
     printf("x = %f, y = %f, z = %f\n", p.x, p.y, p.z);
     return 0;
